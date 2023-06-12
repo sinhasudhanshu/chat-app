0d20d9e03ccd92e095432c44c3ac5b6893a62f64
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-06-12 13:57:09 +0530

    initialize firebase project in react js

---
+++ src/misc/Firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCqBzFj3cpZmLmm7SuMOLCqtKE3G0tsCcU",
+    authDomain: "chat-app-fe47f.firebaseapp.com",
+    projectId: "chat-app-fe47f",
+    storageBucket: "chat-app-fe47f.appspot.com",
+    messagingSenderId: "302368756636",
+    appId: "1:302368756636:web:39ae8871b2a5d4edb72e59",
+    measurementId: "G-212DNNMM5J"
+  }
+
+  const app = firebase.initializeApp(config);
